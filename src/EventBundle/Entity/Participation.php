e1b580db2b77ec4a5d5a7578f422b99e6eb88f3a
elodie05 <e.cantrel@yahoo.fr>
2016-04-25 22:08:04 +0200

    Add Participation entity

---
+++ src/EventBundle/Entity/Participation.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace EventBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Event
+ *
+ * @ORM\Table(name="participation")
+ * @ORM\Entity(repositoryClass="EventBundle\Repository\ParticipcationRepository")
+ */
+class Participation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="EventBundle\Entity\Event")
+     * @ORM\JoinColumn
+     */
+    private $event;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="UserBundle\Entity\User")
+     * @ORM\JoinColumn
+     */
+    private $user;
+    
+
+    /**
+     * @ORM\Column(name="response", type="boolean")
+     */
+    private $response;
+    
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set response
+     *
+     * @param boolean $response
+     * @return Participation
+     */
+    public function setResponse($response)
+    {
+        $this->response = $response;
+
+        return $this;
+    }
+
+    /**
+     * Get response
+     *
+     * @return boolean 
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Set event
+     *
+     * @param \EventBundle\Entity\Event $event
+     * @return Participation
+     */
+    public function setEvent(\EventBundle\Entity\Event $event = null)
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    /**
+     * Get event
+     *
+     * @return \EventBundle\Entity\Event 
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \UserBundle\Entity\User $user
+     * @return Participation
+     */
+    public function setUser(\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \UserBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
