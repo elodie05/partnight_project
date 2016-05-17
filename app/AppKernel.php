fa1aa672e236df042af0429229910561c3527135
elodie05 <e.cantrel@yahoo.fr>
2016-05-17 21:24:53 +0200

    User profile

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new CommonBundle\CommonBundle(),
         	new Avanzu\AdminThemeBundle\AvanzuAdminThemeBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+        		
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
